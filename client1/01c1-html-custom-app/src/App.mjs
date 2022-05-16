38046daf1e258a042da65886a30acea923637ebc
robinmattern <robin.mattern@gmail.com>
2022-05-16 02:34:43 -0400

    Init blank page

---
+++ client1/01c1-html-custom-app/src/App.mjs
@@ -0,0 +1,20 @@
+function  App( ) { 
+
+ return (
+
+//  --------------------------------------------------
+`
+  <div style="height: 100%;" >   
+    <main>
+      <p> This is the page content </p>
+    </main>  
+  </div> 
+`
+    )   // eos return
+//  --------------------------------------------------
+  } // eof App
+//--------------------------------------------------------
+
+ export default App 
+
+// console.log( App() )
---
+++ client1/01c1-html-custom-app/src/index.mjs
@@ -0,0 +1,9 @@
+  import   App          from './App.mjs';
+
+// -------  -----------------   =  ---------------------------------- 
+
+       var  pRoot = document.getElementById( 'root' )
+        
+            pRoot.innerHTML = App( )
+
+// -------  -----------------   =  ---------------------------------- 
